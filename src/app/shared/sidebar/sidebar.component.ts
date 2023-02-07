b81f4124244b6b6dc59acfe8dc61db9d3e75368b
Andres Felipe Guacheta Montaño <andresgumo25@gmail.com>
2023-02-07 17:59:14 -0500

    creacion paises app

---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,13 @@
+import { Component,NgModule } from '@angular/core';
+
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+
+export class SidebarComponent {
+  
+
+}
